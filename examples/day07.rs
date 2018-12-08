8de1342db1098ac1efec4fb4180f2b47161a6947
liyuntao <liyuntao58607@gmail.com>
2018-12-08 15:40:55 +0800

    finished day7 q2

--- examples/day07.rs
+++ examples/day07.rs
@@ -3,11 +3,19 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 const MAX_STEPS: usize = 26;
+const WORKER_NUMBERS: usize = 5;
+const BASE_PROCESS_TIME: u32 = 60;
+
 //const MAX_STEPS: usize = 6;
+//const WORKER_NUMBERS: usize = 2;
+//const BASE_PROCESS_TIME: u32 = 0;
 
 #[derive(Debug, Clone)]
 struct Step {
-    handled: bool,
+    q1_handled: bool,
+    // after done the step, mark it as true
+    q2_handled: bool,
+    // after worker handled the step, mark it as true
     from: Vec<usize>,
     to: Vec<usize>,
 }
@@ -19,9 +27,10 @@ fn char2usize(c: char) -> usize {
 fn parse_all(vec: Vec<String>) -> Vec<Step> {
     let mut dots: Vec<Step> = vec![
         Step {
-            handled: false,
+            q1_handled: false,
+            q2_handled: false,
             from: Vec::new(),
-            to: Vec::new()
+            to: Vec::new(),
         };
         MAX_STEPS
     ];
@@ -37,20 +46,8 @@ fn parse_all(vec: Vec<String>) -> Vec<Step> {
     dots
 }
 
-fn main() {
-    let path = format!("./input/{}", "day07.txt");
-
-    let vec: Vec<String> = BufReader::new(File::open(path).unwrap())
-        .lines()
-        .map(|l| l.expect("Could not parse line"))
-        .collect();
-
-    let mut steps = parse_all(vec);
+fn q1(steps: &mut Vec<Step>) {
     let mut result = String::new();
-
-
-    // q1
-//    steps.iter().for_each(|x| println!("{:?}", x));
     let mut s_queue = HashSet::new();
     for i in 0..MAX_STEPS {
         if steps[i].from.is_empty() {
@@ -67,7 +64,7 @@ fn main() {
                     steps[*index]
                         .from
                         .iter()
-                        .filter(|&idx| !steps[*idx].handled)
+                        .filter(|&idx| !steps[*idx].q1_handled)
                         .collect::<Vec<&usize>>()
                         .is_empty()
                 })
@@ -80,7 +77,7 @@ fn main() {
         };
 
         // mark as handled
-        steps[least_available_step_id].handled = true;
+        steps[least_available_step_id].q1_handled = true;
 
         // add all 'to' element to s_queue
         steps[least_available_step_id].to.iter().for_each(|&id| {
@@ -92,4 +89,113 @@ fn main() {
     }
 
     println!("result of q01 is {}", result);
+}
+
+#[derive(Debug, Clone, PartialEq)]
+enum WorkerState {
+    IDLE,
+    OnProcess(usize /*stepId*/, u32 /*seconds left*/),
+}
+
+#[derive(Debug, Clone)]
+struct Worker {
+    state: WorkerState,
+}
+
+impl Worker {
+    fn tick(&mut self) -> Option<usize> {
+        match self.state {
+            WorkerState::OnProcess(step_id, left) => {
+                if left == 1 {
+                    self.state = WorkerState::IDLE;
+                    Some(step_id)
+                } else {
+                    self.state = WorkerState::OnProcess(step_id, left - 1);
+                    None
+                }
+            }
+            WorkerState::IDLE => None,
+        }
+    }
+}
+
+fn is_all_workers_idle(workers: &Vec<Worker>) -> bool {
+    workers
+        .iter()
+        .fold(true, |b, ref worker| b && worker.state == WorkerState::IDLE)
+}
+
+fn q2(steps: &mut Vec<Step>) {
+    let mut seconds: i32 = 0;
+    let mut task_queue = HashSet::new();
+    for i in 0..MAX_STEPS {
+        if steps[i].from.is_empty() {
+            task_queue.insert(i);
+        }
+    }
+
+    let mut workers = vec![
+        Worker {
+            state: WorkerState::IDLE
+        };
+        WORKER_NUMBERS
+    ];
+
+    while !task_queue.is_empty() || !is_all_workers_idle(&workers) {
+        workers.iter_mut().for_each(|wk| {
+            if let Some(finished_step_id) = wk.tick() {
+                steps[finished_step_id].q2_handled = true;
+
+                // add step's 'to' to task_queue
+                steps[finished_step_id].to.iter().for_each(|&id| {
+                    task_queue.insert(id);
+                });
+            }
+        });
+
+        workers.iter_mut().for_each(|wk| {
+            if wk.state == WorkerState::IDLE {
+                // fetch a leastId && available step. (available == all from steps are q2_handled)
+                // may not existed
+                let least_available_step_id_option: Option<usize> = task_queue
+                    .iter()
+                    .filter(|&index| {
+                        steps[*index]
+                            .from
+                            .iter()
+                            .filter(|&idx| !steps[*idx].q2_handled)
+                            .collect::<Vec<&usize>>()
+                            .is_empty()
+                    })
+                    .min()
+                    .map(|a| a.to_owned());
+
+                if let Some(least_available_step_id) = least_available_step_id_option {
+                    // delete from task_queue(pop out)
+                    task_queue.retain(|&x| x != least_available_step_id);
+                    // assign step to worker
+                    let step_processing_time: u32 =
+                        least_available_step_id as u32 + 1 + BASE_PROCESS_TIME;
+                    wk.state =
+                        WorkerState::OnProcess(least_available_step_id, step_processing_time);
+                }
+            }
+        });
+        seconds += 1;
+    }
+    println!("result of q02 is {}", seconds - 1);
+}
+
+fn main() {
+    let path = format!("./input/{}", "day07.txt");
+
+    let vec: Vec<String> = BufReader::new(File::open(path).unwrap())
+        .lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect();
+
+    let mut steps = parse_all(vec);
+    //        steps.iter().for_each(|x| println!("{:?}", x));
+    q1(&mut steps);
+    q2(&mut steps);
 }
