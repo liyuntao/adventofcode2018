a7fe579ef53e4f24f416cd3dc95fd65649b26528
liyuntao <liyuntao58607@gmail.com>
2018-12-10 09:58:15 +0800

    finished day08 q2

--- examples/day08.rs
+++ examples/day08.rs
@@ -1,4 +1,3 @@
-use std::collections::VecDeque;
 use std::fs::File;
 use std::io::Read;
 
@@ -6,11 +5,12 @@ use std::io::Read;
 struct Node {
     children: Vec<Node>,
     metadata: Vec<u32>,
+    value: u32,
 }
 
 fn parse_node(input: &Vec<u32>, start_pos: usize) -> (Node, usize, u32) {
     let mut pos = start_pos;
-    let mut sum = 0;
+    let mut meta_sum = 0;
 
     let child_count = input[pos];
     pos += 1;
@@ -20,21 +20,31 @@ fn parse_node(input: &Vec<u32>, start_pos: usize) -> (Node, usize, u32) {
     let mut res = Node {
         children: Vec::new(),
         metadata: Vec::new(),
+        value: 0,
     };
 
-    for i in 0..child_count {
+    for _i in 0..child_count {
         let (child, next_start_pos, child_sum) = parse_node(input, pos);
-        sum += child_sum;
+        meta_sum += child_sum;
         res.children.push(child);
         pos = next_start_pos;
     }
 
-    for i in 0..meta_count {
-        sum += input[pos];
+    for _i in 0..meta_count {
+        meta_sum += input[pos];
         res.metadata.push(input[pos]);
+
+        if res.children.is_empty() {
+            res.value += input[pos];
+        } else {
+            let index = input[pos] as usize;
+            if index > 0 && index <= res.children.len() {
+                res.value += res.children[index - 1].value;
+            }
+        }
         pos += 1;
     }
-    (res, pos, sum)
+    (res, pos, meta_sum)
 }
 
 fn main() {
@@ -49,8 +59,7 @@ fn main() {
             .collect()
     };
 
-    // q1
-    let (root, _, sum) = parse_node(&input, 0);
-    //    println!("{:?}", root);
-    println!("result of q01 is {}", sum);
+    let (root, _, meta_sum) = parse_node(&input, 0);
+    println!("result of q01 is {}", meta_sum);
+    println!("result of q02 is {}", root.value);
 }
