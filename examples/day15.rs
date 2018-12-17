f53b5bec9bff13fd47c1c008257643409cb11d01
liyuntao <liyuntao58607@gmail.com>
2018-12-17 14:47:34 +0800

    finished day15 q2

--- examples/day15.rs
+++ examples/day15.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -9,32 +10,8 @@ struct Assertion {
 }
 
 impl Assertion {
-    fn try_assert(&self, id: usize) -> bool {
-        let mut gen: [usize; 4] = Default::default();
-        gen.copy_from_slice(&self.after);
-        let be = &self.before;
-        let cmd = &self.cmd;
-
-        match id {
-            0 => gen[cmd.3] = be[cmd.1] + be[cmd.2],
-            1 => gen[cmd.3] = be[cmd.1] + cmd.2,
-            2 => gen[cmd.3] = be[cmd.1] * be[cmd.2],
-            3 => gen[cmd.3] = be[cmd.1] * cmd.2,
-            4 => gen[cmd.3] = be[cmd.1] & be[cmd.2],
-            5 => gen[cmd.3] = be[cmd.1] & cmd.2,
-            6 => gen[cmd.3] = be[cmd.1] | be[cmd.2],
-            7 => gen[cmd.3] = be[cmd.1] | cmd.2,
-            8 => gen[cmd.3] = be[cmd.1],
-            9 => gen[cmd.3] = cmd.1,
-            10 => gen[cmd.3] = if cmd.1 > be[cmd.2] { 1 } else { 0 },
-            11 => gen[cmd.3] = if be[cmd.1] > cmd.2 { 1 } else { 0 },
-            12 => gen[cmd.3] = if be[cmd.1] > be[cmd.2] { 1 } else { 0 },
-            13 => gen[cmd.3] = if cmd.1 == be[cmd.2] { 1 } else { 0 },
-            14 => gen[cmd.3] = if be[cmd.1] == cmd.2 { 1 } else { 0 },
-            15 => gen[cmd.3] = if be[cmd.1] == be[cmd.2] { 1 } else { 0 },
-            _ => {}
-        };
-
+    fn try_assert(&self, index: usize) -> bool {
+        let gen = execute(&self.before, self.cmd, index);
         gen[0] == self.after[0]
             && gen[1] == self.after[1]
             && gen[2] == self.after[2]
@@ -42,7 +19,34 @@ impl Assertion {
     }
 }
 
-fn main() {
+fn execute(registers: &[usize; 4], cmd: (usize, usize, usize, usize), index: usize) -> [usize; 4] {
+    let mut gen: [usize; 4] = Default::default();
+    gen.copy_from_slice(registers);
+    let be = registers;
+
+    match index {
+        0 => gen[cmd.3] = be[cmd.1] + be[cmd.2],
+        1 => gen[cmd.3] = be[cmd.1] + cmd.2,
+        2 => gen[cmd.3] = be[cmd.1] * be[cmd.2],
+        3 => gen[cmd.3] = be[cmd.1] * cmd.2,
+        4 => gen[cmd.3] = be[cmd.1] & be[cmd.2],
+        5 => gen[cmd.3] = be[cmd.1] & cmd.2,
+        6 => gen[cmd.3] = be[cmd.1] | be[cmd.2],
+        7 => gen[cmd.3] = be[cmd.1] | cmd.2,
+        8 => gen[cmd.3] = be[cmd.1],
+        9 => gen[cmd.3] = cmd.1,
+        10 => gen[cmd.3] = if cmd.1 > be[cmd.2] { 1 } else { 0 },
+        11 => gen[cmd.3] = if be[cmd.1] > cmd.2 { 1 } else { 0 },
+        12 => gen[cmd.3] = if be[cmd.1] > be[cmd.2] { 1 } else { 0 },
+        13 => gen[cmd.3] = if cmd.1 == be[cmd.2] { 1 } else { 0 },
+        14 => gen[cmd.3] = if be[cmd.1] == cmd.2 { 1 } else { 0 },
+        15 => gen[cmd.3] = if be[cmd.1] == be[cmd.2] { 1 } else { 0 },
+        _ => {}
+    };
+    gen
+}
+
+fn solution() {
     let path = format!("./input/{}", "day15_q1.txt");
 
     let vec: Vec<String> = BufReader::new(File::open(path).unwrap())
@@ -90,4 +94,78 @@ fn main() {
         .count();
 
     println!("result of q01 is {}", q1);
+
+    // gen opcode->Set(indexes) mapping
+    let mut opcode_mapping: Vec<HashSet<usize>> = vec![HashSet::new(); 16];
+
+    asserts.iter().for_each(|ref ass| {
+        let opcode = ass.cmd.0;
+        let mut index_set: HashSet<usize> = HashSet::new();
+        (0..=15usize).for_each(|idx| {
+            if ass.try_assert(idx) {
+                index_set.insert(idx);
+            }
+        });
+        let un_init = opcode_mapping[opcode].is_empty();
+        if un_init {
+            opcode_mapping[opcode] = index_set;
+        } else {
+            opcode_mapping[opcode] = opcode_mapping[opcode]
+                .intersection(&index_set)
+                .map(|&x| x)
+                .collect::<HashSet<usize>>();
+        }
+    });
+    println!("DEBUG {:?}", opcode_mapping);
+
+    // repeat deduction until every Set.len() == 1
+    let mut handled: HashSet<usize> = HashSet::new();
+    while opcode_mapping.iter().any(|set| set.len() != 1) {
+        for set in opcode_mapping.iter() {
+            if set.len() == 1 {
+                handled.insert(*set.iter().next().unwrap());
+            }
+        }
+        for set in opcode_mapping.iter_mut() {
+            if set.len() > 1 {
+                handled.iter().for_each(|num| {
+                    set.remove(&num);
+                })
+            }
+        }
+    }
+    println!("DEBUG {:?}", opcode_mapping);
+
+    // normalize
+    let opcode_mapping = opcode_mapping
+        .into_iter()
+        .map(|set| set.into_iter().next().unwrap())
+        .collect::<Vec<usize>>();
+    println!("DEBUG {:?}", opcode_mapping);
+
+    // parsing q2 input
+    let path = format!("./input/{}", "day15_q2.txt");
+    let instructions: Vec<Vec<usize>> = BufReader::new(File::open(path).unwrap())
+        .lines()
+        .map(|l| l.expect("Could not parse line"))
+        .map(|line| {
+            line.split(' ')
+                .map(|u| u.parse::<usize>().expect(""))
+                .collect()
+        })
+        .collect();
+
+    let mut register: [usize; 4] = [0usize; 4];
+    for ins in instructions {
+        register = execute(
+            &register,
+            (ins[0], ins[1], ins[2], ins[3]),
+            opcode_mapping[ins[0]],
+        );
+    }
+    println!("result of q02 is {}", register[0]);
+}
+
+fn main() {
+    solution();
 }
